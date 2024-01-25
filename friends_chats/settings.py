a5bebf55bb04fa4ed14fcf9e0a89d2c4b294dcd8
Muntasir00 <muntasir690000@gmail.com>
2024-01-26 01:54:08 +0600

    access token duration increased

--- friends_chats/settings.py
+++ friends_chats/settings.py
@@ -46,6 +46,7 @@ JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
+ACCESS_TOKEN_LIFETIME = 15 * 24 * 60 * 60 
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'friends_chats.serializers.CurrentUserSerializer'
